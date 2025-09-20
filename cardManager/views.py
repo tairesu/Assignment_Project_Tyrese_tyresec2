2576a46780b29cbb402334e89fe6d8dae40cd968
tyroPatel <tyresecook25@yahoo.com>
2025-09-20 16:04:29 -0500

    Added reroute logic to card detail view (based on card attributes)

--- cardManager/views.py
+++ cardManager/views.py
@@ -7,8 +7,23 @@ from cardManager.models import (
 # Create your views here.
 def card_detail(request,card_token):
 	print('card_detail request: ', request)
-	cards = Card.objects.filter(token=card_token)
-	context = {"cards":cards}
+	card = Card.objects.filter(token=card_token)[0]
+	show_profile = card.show_profile
+	is_owned = not (card.user == None)
+	is_redirecting = not (card.reroute_url == None)
+	context = {"card":card}
+
+	if is_owned and not show_profile:
+		context["decision"] = f"Redirecting to {card.reroute_url}"
+
+	elif is_owned and show_profile: 
+		context["decision"] = f"Redirecting to {card.user}'s Profile"
+
+	elif not is_owned:
+		context["decision"] = f"Redirecting to {card_token}/activate"
+	else:
+		context["decision"] = f"Card token not found"
+
 	template = loader.get_template("cardManager/base.html")
 	output = template.render(context, request)
 	return HttpResponse(output)
