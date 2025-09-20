03614b57d907a23c9549267c02823a10b12aea50
tyroPatel <tyresecook25@yahoo.com>
2025-09-20 15:41:19 -0500

    Initialized Card Detail View

--- cardManager/views.py
+++ cardManager/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
+from cardManager.models import (
+	Card,
+)
 # Create your views here.
+def card_detail(request,card_token):
+	print('card_detail request: ', request)
+	cards = Card.objects.filter(token=card_token)
+	context = {"cards":cards}
+	template = loader.get_template("cardManager/base.html")
+	output = template.render(context, request)
+	return HttpResponse(output)
--- tcDigitalCards/urls.py
+++ tcDigitalCards/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cardManager import views
 
 urlpatterns = [
+    path('card/<str:card_token>/', views.card_detail, name='all_cards'),
     path('admin/', admin.site.urls),
 ]
