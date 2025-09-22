7079c6afc926e0c3fcf1a804b809dff868e90869
tyroPatel <tyresecook25@yahoo.com>
2025-09-21 20:55:43 -0500

    payment_integration.js gets publishable key

--- cardManager/static/cardManager/js/payment_integration.js
+++ cardManager/static/cardManager/js/payment_integration.js
@@ -1,1 +1,10 @@
 console.log("JS has been summoned")
+
+fetch('/config/')
+	.then((result) => {
+		return result.json()
+	})
+	.then((data)=>{
+		const stripe = Stripe(data.publishable_key)
+		console.log('Pub key @ /config/:', data.publishable_key)  
+	})
