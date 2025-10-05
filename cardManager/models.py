1e866f8abc81b6e87d8c0e785a10b8d96a5a475b
tyroPatel <tyresecook25@yahoo.com>
2025-10-05 18:47:57 -0500

    Usage model is now insert only

--- cardManager/models.py
+++ cardManager/models.py
@@ -76,7 +76,7 @@ class Card(models.Model):
 
 
 class Usage(models.Model):
-	card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name='usage')
+	card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name='usage',editable=False)
 	date_used = models.DateTimeField(default=timezone.now, editable=False)
 
 	def __str__(self):
