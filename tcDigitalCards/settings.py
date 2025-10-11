84c9fe1d7c501c8d68be4d7a4d57564226a59a72
Tairesu <tyresecook25@yahoo.com>
2025-10-11 02:48:29 -0500

    Config settings to properly use Static and media

--- tcDigitalCards/settings.py
+++ tcDigitalCards/settings.py
@@ -117,9 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'cardManager' 
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR  / 'media'
+MEDIA_ROOT = BASE_DIR  / "media"
 
 LOGIN_URL = '/login/'
 LOGOUT_URL = '/logout/'
