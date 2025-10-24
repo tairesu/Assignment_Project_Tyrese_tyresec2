a5771383e3135da26e8be18e10f896595600d622
tyroPatel <tyresecook25@yahoo.com>
2025-10-24 03:07:18 -0500

    forms js: removes danger visuals from card update form when show_profile is checked

--- static/js/forms.js
+++ static/js/forms.js
@@ -1,9 +1,19 @@
 /* Assignment 8.5 */
 
 const redirectDiv = document.querySelector("#redirectDiv");
+const redirectUrl = document.querySelector("#id_reroute_url");
 
 function toggleRedirectDiv(radioValue) {
 	let show_profile = document.querySelector("input#yes").checked;
-	if ( show_profile ) { redirectDiv.classList.add("dim-out")}
+	if ( show_profile ) {
+		redirectDiv.classList.add("dim-out");
+		redirectUrl.value = "";
+		redirectUrl.style.borderColor = "";
+		if (document.querySelector("#redirectDiv ul.errorlist") != null ){
+			document.querySelector("#redirectDiv ul.errorlist").remove();
+		}
+
+
+	}
 	else if (!show_profile) { redirectDiv.classList.remove("dim-out") }
 }
