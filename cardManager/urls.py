b3b09314ed860fe33bdcc017b40565af0c61f3ef
tyroPatel <tyresecook25@yahoo.com>
2025-10-29 14:30:55 -0500

    Moved url paths from project level urls

--- cardManager/urls.py
+++ cardManager/urls.py
@@ -1,3 +1,14 @@
 from django.urls import path, include
 from cardManager import views 
 
+urlpatterns = [
+	path('card/<str:card_token>/', views.card_detail, name='card_view'),
+	path('card/<str:card_token>/activate/', views.CardDetail.as_view(), name='card_activate_view'),
+	path('card/<str:card_token>/fbv-update/', views.card_update, name='card_update_view'),
+	path('card/<str:card_token>/class-update/', views.CardUpdate.as_view(), name='card_class_update_view'),
+	path('profile/<str:profile_slug>/', views.ProfileDetail.as_view(), name='profile_view'),
+	path('profile/<str:profile_slug>/update', views.ProfileUpdate.as_view(), name='profile_update_view'),
+	path('stats/', views.Stats.as_view(), name='stats_view'),
+	path('dashboard/', views.UserDashboard.as_view(), name='dashboard_view'),
+	path('stats/fetch_plotly_data', views.config_plotly),
+]
--- tcDigitalCards/urls.py
+++ tcDigitalCards/urls.py
@@ -22,15 +22,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('card/<str:card_token>/', views.card_detail, name='card_view'),
-    path('card/<str:card_token>/activate/', views.CardDetail.as_view(), name='card_activate_view'),
-    path('card/<str:card_token>/fbv-update/', views.card_update, name='card_update_view'),
-    path('card/<str:card_token>/class-update/', views.CardUpdate.as_view(), name='card_class_update_view'),
-    path('profile/<str:profile_slug>/', views.ProfileDetail.as_view(), name='profile_view'),
-    path('profile/<str:profile_slug>/update', views.ProfileUpdate.as_view(), name='profile_update_view'),
-    path('stats/', views.Stats.as_view(), name='stats_view'),
-    path('dashboard/', views.UserDashboard.as_view(), name='dashboard_view'),
-    path('stats/fetch_plotly_data', views.config_plotly),
+    path('', include('cardManager.urls')),
     path('admin/', admin.site.urls),
 ]
 
