9cce2d24647a5381fdcdd62034e6638b02692e7e
tyroPatel <tyresecook25@yahoo.com>
2025-11-12 23:38:46 -0600

    Set logout and login redirect url

--- tcDigitalCards/settings.py
+++ tcDigitalCards/settings.py
@@ -123,6 +123,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR  / "media"
 
 LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = 'dashboard_view'
 LOGOUT_URL = '/logout/'
 LOGOUT_REDIRECT_URL = 'login_view'
 
