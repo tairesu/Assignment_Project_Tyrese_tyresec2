e8b052d26a995746afe6a386215bb57d31b7cc9d
tyroPatel <tyresecook25@yahoo.com>
2025-11-12 18:44:53 -0600

    Renamed stats urlpattern to reports

--- cardManager/urls.py
+++ cardManager/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 	path('profile/slug/<str:profile_slug>/', views.ProfileDetail.as_view(), name='profile_view'),
 	path('profile/slug/<str:profile_slug>/update', views.ProfileUpdate.as_view(), name='profile_update_view'),
 	path('dashboard/', views.UserDashboard.as_view(), name='dashboard_view'),
-	path('stats/', views.Stats.as_view(), name='stats_view'),
+	path('reports/', views.Stats.as_view(), name='stats_view'),
 	path('stats/fetch_plotly_data', views.config_plotly),
 	path('api/daily_usage/', views.daily_usage, name='api_daily_usage_view'),
 	path('api/daily_usage/bar_graph.png', views.daily_usage_png, name='daily_usage_graph_png_view'),
