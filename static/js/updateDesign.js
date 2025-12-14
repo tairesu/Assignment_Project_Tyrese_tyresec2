59ca3fea63c81456420de096bf373912b666fcad
tyroPatel <tyresecook25@yahoo.com>
2025-12-14 11:47:54 -0600

    clicking upload a design card reveals #uploadFile

--- static/js/updateDesign.js
+++ static/js/updateDesign.js
@@ -2,9 +2,17 @@ const card = document.querySelector('#card');
 const design_input = document.querySelector("input#design_choice");
 
 function setDesignChoice(button_ele) {
+    hideUploadFile();
     child_img_id = button_ele.querySelector("img").dataset['id'];
     child_img_src = button_ele.querySelector("img").src;
     /* Update Card image */
     card.src = child_img_src;
     design_input.value = child_img_id;
+}
+
+function showUploadFile() {
+    $('#uploadFile').animate({height:'show'},200);
+}
+function hideUploadFile() {
+    $('#uploadFile').animate({height:'hide'},200);
 }
