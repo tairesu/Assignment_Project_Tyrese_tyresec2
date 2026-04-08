d31b9171b90d463aa3d714a8f75eea32dde28380
tairesu <tyresecook25@yahoo.com>
2026-04-07 21:37:25 -0500

    added lost production settings using scraphounds project

--- tcDigitalCards/settings/production.py
+++ tcDigitalCards/settings/production.py
@@ -9,8 +9,35 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'tcdigital.pythonanywhere.com']
+ALLOWED_HOSTS = ['0.0.0.0','localhost', '127.0.0.1', 'tcdigital.blake4it.com']
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+USE_X_FORWARDED_HOST = False
+CSRF_TRUSTED_ORIGINS = [
+    "https://tcdigital.blake4it.com",
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "loggers": {
+        "django.security.DisallowedHost": {"handlers": ["console"], "level": "ERROR", "propagate": False},
+        "django.request": {"handlers": ["console"], "level": "ERROR", "propagate": True},
+    },
+}
