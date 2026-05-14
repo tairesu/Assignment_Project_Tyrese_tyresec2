32431e268d52e47ea23934af7896605f372e594a
tairesu <tyresecook25@yahoo.com>
2026-05-14 18:54:04 -0500

    add: setting deltaX, currentX, isSwiping

--- static/js/rotating_card.js
+++ static/js/rotating_card.js
@@ -4,7 +4,7 @@ const scene = document.querySelector('#card-scene');
 const card_designs = document.querySelector('#card-designs');
 
 /* To be changed when pointerdown happens */
-let startX = 0;
+let currentX = 0;
 
 /* Bool made available to all events */
 let isSwiping = false;
@@ -12,14 +12,19 @@ let isSwiping = false;
 /* When user touches the scene */
 scene.addEventListener('pointerdown', (e) => {
     console.log('Pointer X Down @', e.clientX);
+    isSwiping = true;
+    currentX = e.clientX;
 })
 
 /* When user drags inside the scene */
 scene.addEventListener('pointermove', (e) => {
-    console.log('Now @ ', e.clientX);
+    console.log('\tNow @ ', e.clientX);
 })
 
 /* When user leaves the scene */
 scene.addEventListener('pointerup', (e) => {
     console.log('Pointer X Left @ ', e.clientX);
+    isSwiping = false;
+    deltaX = e.clientX - currentX;
+    console.log('Delta X was:', deltaX, ' units')
 })
