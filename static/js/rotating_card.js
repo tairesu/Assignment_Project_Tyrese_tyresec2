2ec6d241e90fa02631eb6b39c9f9102bb85c3967
tairesu <tyresecook25@yahoo.com>
2026-05-14 21:24:24 -0500

    add: card rotate ui

--- static/js/rotating_card.js
+++ static/js/rotating_card.js
@@ -5,26 +5,39 @@ const card_designs = document.querySelector('#card-designs');
 
 /* To be changed when pointerdown happens */
 let currentX = 0;
-
-/* Bool made available to all events */
+let currentRotation = 0;
 let isSwiping = false;
 
+/* Bool made available to all events */
+
+const getRotateY = () => {
+    rotateY_value = card_designs.style.transform; // RotateY(5deg)
+    rotateY_int = rotateY_value.match('-?[0-9]{1,}') && parseInt(rotateY_value.match('-?[0-9]{1,}')[0]);
+    return rotateY_int;
+};
+const setRotateY = (deltaRotate) => {
+    currentRotation = getRotateY() + (deltaRotate);
+    card_designs.style.transform = `rotateY(${ currentRotation }deg)`;
+};
 /* When user touches the scene */
 scene.addEventListener('pointerdown', (e) => {
-    console.log('Pointer X Down @', e.clientX);
+    //console.log('Pointer X Down @', e.clientX);
     isSwiping = true;
     currentX = e.clientX;
 })
 
 /* When user drags inside the scene */
 scene.addEventListener('pointermove', (e) => {
-    console.log('\tNow @ ', e.clientX);
+    if (!isSwiping) return;
+    deltaX = e.clientX - currentX;
+    currentX = e.clientX;
+    setRotateY(deltaX)
+    console.log(' pointerx=', currentX, '\nDelta pointerx=:', deltaX,  '\ne.pageX=', e.pageX, '\nele rotation=', currentRotation)
 })
 
 /* When user leaves the scene */
 scene.addEventListener('pointerup', (e) => {
     console.log('Pointer X Left @ ', e.clientX);
     isSwiping = false;
-    deltaX = e.clientX - currentX;
-    console.log('Delta X was:', deltaX, ' units')
+    
 })
